36098f1ad889c4234fafd268d3da329fa455e834
SheillyR <sheillyrolo@gmail.com>
2021-02-03 12:26:49 -0500

    Orders view

--- burger-queen/src/app/views/orders/orders.component.ts
+++ burger-queen/src/app/views/orders/orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { OrdersResponseI } from '../../models/orders-response.model';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.sass']
 })
 export class OrdersComponent implements OnInit {
+  
+  orders!: OrdersResponseI[];
+  ordersList!: OrdersResponseI[];
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.bringOrders();
+  }
+  deliverOrder(id: string, status:string){
+    this.api.putOrder(id, status).subscribe(data => {
+      this.bringOrders();
+    })
+  }
+  //Metodo traer ordernes
+  bringOrders(){
+    this.api.getOrders().subscribe(data => {
+      this.orders=data;
+      this.ordersList = this.orders.filter(item =>  item.status === "delivering");
+    })
   }
 
 }
